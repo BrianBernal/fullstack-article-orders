56ac0b9abf4822690df490c31e7692c8dcbf4416
Brian Bernal <brianbernal27@gmail.com>
2023-05-11 10:32:06 -0500

    Move LanguageSelector into navbar folder

--- src/app/layout/navbar/desktop/Navbar.tsx
+++ src/app/layout/navbar/desktop/Navbar.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import LanguageSelector from "../../languageSelector/LanguageSelector";
+import LanguageSelector from "./languageSelector/LanguageSelector";
 
 function Navbar() {
   return (
