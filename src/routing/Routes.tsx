356adc4529c39dff8b8a21daa44a37afa197ceb0
Brian Bernal <brianbernal27@gmail.com>
2023-05-15 15:52:39 -0500

    Rename routes import

--- src/routing/Routes.tsx
+++ src/routing/Routes.tsx
@@ -1,5 +1,5 @@
 // libraries
-import { BrowserRouter, Route, Routes as ReactRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes as ReactRoutes } from "react-router-dom";
 
 // components
 import Articles from "@/features/articles/Articles";
@@ -10,14 +10,14 @@ import Layout from "@/app/layout/Layout";
 function Routes() {
   return (
     <BrowserRouter>
-      <ReactRouter>
+      <ReactRoutes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Articles />} />
           <Route path="articles" element={<Articles />} />
           <Route path="orders" element={<Orders />} />
           <Route path="*" element={<Error404 />} />
         </Route>
-      </ReactRouter>
+      </ReactRoutes>
     </BrowserRouter>
   );
 }
